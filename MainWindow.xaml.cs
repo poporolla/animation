b685b4847c283c6f7c275f6e7b0c8e13e0606afa
poporolla <mansur_17@mail.ru>
2020-01-13 19:26:14 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Ellipse_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			Ellipse ellipse = sender as Ellipse;
+			Border border = ExtElem;
+
+			//DoubleAnimation marAnimation = new DoubleAnimation();
+
+			//перемещение круга
+			ThicknessAnimation ellipseMargin = new ThicknessAnimation();
+			ellipseMargin.From = ellipse.Margin;
+			ellipseMargin.To = new Thickness(55, 0, 0, 0);
+			ellipseMargin.Duration = TimeSpan.FromSeconds(1.5);
+			ellipse.BeginAnimation(Ellipse.MarginProperty, ellipseMargin);
+
+
+		}
+	}
+}
